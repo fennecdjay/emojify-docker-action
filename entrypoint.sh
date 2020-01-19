3e5074274231d866ea0e9e792ce83f1858ff67b3
Jérémie Astor <astor.jeremie@wanadoo.fr>
2020-01-19 23:02:47 +0100

    also process mdr files

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 curl https://raw.githubusercontent.com/mrowa44/emojify/master/emojify -o ./emojify
 chmod +x ./emojify
 
-for file in "$1"/*.md
+for file in "$1"/*.md "$1"/*.mdr
 do
   cat "$file" | ./emojify > "$file.tmp"
   mv "$file.tmp" "$file"
